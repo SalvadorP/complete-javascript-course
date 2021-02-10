3c9594a8cb48cb051d595be85189a81b838a1f0e
Salvador P <salvadorperezd@gmail.com>
2021-02-10 16:15:46 +0100

    [CHANGE] JS Snippets. [CHANGE] Guess my number code.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,44 @@
-'use strict';
+"use strict";
+//.Wait for the document to load then do things.
+document.addEventListener("DOMContentLoaded", function (event) {
+  let number = 0;
+  let button = document.querySelector(".check");
+  let againBtn = document.querySelector(".again");
+  let message = document.querySelector(".message");
+  let body = document.querySelector("body");
+  let score = document.querySelector(".score");
+
+  const randomNumber = () => {
+    number = Math.floor(Math.random() * 20);
+    console.info(number);
+  };
+  const decreaseScore = () => {
+    let scoreValue = parseInt(score.innerHTML);
+    scoreValue--;
+    score.innerHTML = scoreValue;
+  };
+  const checkNumber = () => {
+    let guess = document.querySelector(".guess");
+    const guessValue = Number(guess.value);
+    if (guessValue == number) {
+      message.innerHTML = "match!";
+      body.style.backgroundColor = "green";
+    }
+    if (guessValue > number) {
+      message.innerHTML = "lower";
+      decreaseScore();
+    }
+    if (guessValue < number) {
+      message.innerHTML = "upper";
+      decreaseScore();
+    }
+  };
+  againBtn.addEventListener("click", () => {
+    body.style.backgroundColor = "#222";
+    score.innerHTML = 20;
+    randomNumber();
+  });
+  button.addEventListener("click", checkNumber);
+
+  randomNumber();
+});

c993096a03846cde2843ef17d64a257f993a4e94
Salvador P <salvadorperezd@gmail.com>
2021-02-10 09:04:41 +0100

    [CHANGE] Added snippets and config fro prettier.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+let x = 38;
+if (true) {
+  x = 2;
+}
+console.log(x);
+
+let myfunc = () => {
+  x += 2;
+  console.warn(x);
+};
+
+myfunc();
